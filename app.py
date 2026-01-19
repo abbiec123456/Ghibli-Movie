57d029bed24abd43603694c56e858ddd467885c6
stephen1k <stephen1kuria@gmail.com>
2026-01-19 15:50:56 +0000

    initial booking form page. work in progress

--- app.py
+++ app.py
@@ -85,6 +85,9 @@ def logout():
     session.clear()
     return redirect(url_for('customer_login'))
 
+@app.route('/book')
+def booking():
+    return render_template("booking.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
